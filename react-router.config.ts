9d7a536fffc1bf082afb1aa934eadf114bb0655d
cris-guedes <cristianguedes136@gmail.com>
2026-01-09 23:09:52 -0300

    wip

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,4 +5,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  async prerender() {
+    return ["/", "/home2", "/galeria", "/sobre", "/contato"];
+  },
 } satisfies Config;
